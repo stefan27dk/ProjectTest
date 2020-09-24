f7fc9c649a12b830da2c3f12ebff5651adfd4fba
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-09-24 18:51:03 +0200

    Add project files.

---
+++ ProjectTestAPI/Controllers/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTestAPI.Controllers
+{
+    public class Student
+    {
+        public string name;
+    }
+}
---
+++ ProjectTestAPI/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private List<string> names;
+
+        public StudentController()
+        {
+            names = new List<string> { "BOb", "AA" };
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetStudents(int id)
+        {  
+            return Ok(names[id]);
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> Post( Student student) 
+        { 
+            // Oprettelse i Db
+            // If fejl return bad request(....)
+            //else OK
+            return Ok(); 
+        
+        }
+    }
+}
